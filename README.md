b8c2afaf639a7df39f62c31ca07769ad926a6097
mathurkr <kavi.mathur121@gmail.com>
2021-12-27 01:51:16 -0800

    Added tailwindCSS and postCSS, added filler content and nav

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ This is meant to be a replacement for my current portfolio website, but I'm just
 https://megzari.com/blog/about_this_site
 
 Source Code: https://megzari.com/blog/about_this_site/
+
+## Using TailwindCSS with Svelte
+Source: https://levelup.gitconnected.com/how-to-use-svelte-kit-with-tailwind-css-jit-just-in-time-compilation-bc04c0c9ec17
+- changed references to `purge` to `content`
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,15 @@
-<script lang="ts">
-
+<script>
+    import "../styles/tailwind-output.css";
 </script>
 
-<div>
-    <main class="h-full">
+<div class="bg-gradient-to-r from-slate-500 to-blue-500">
+    <img src="static/favicon.png">
+    <h1 class="text-lg text-center">Kavi Mathur</h1>
+    <nav class="text-center">
+        <a href="/" class="text-center">Home</a>
+    </nav>
+    <main class="mx-20 ">
         <slot />
     </main>
 
 </div>
-
-<style>
-
-</style>
